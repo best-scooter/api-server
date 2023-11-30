f2245ea28963360f08911b4ffcba4e284408a46d
DavidPalmgren <swemultigamers@gmail.com>
2023-11-30 16:43:51 +0100

    Update OAuthMobile.ts

--- src/models/OAuthMobile.ts
+++ src/models/OAuthMobile.ts
@@ -1,1 +1,22 @@
+//import EnvVars from '../constants/EnvVars';
+import { OAuthApp, App, } from 'octokit'
 
+// **** Instansiate the oauth **** //
+
+// const clientId = EnvVars.OAuth.ClientId;
+// const clientSecret = EnvVars.OAuth.ClientSecret;
+//const clientId = process.env.OAUTH_CLIENT_ID?.toString() ?? ''
+//const clientSecret = process.env.OAUTH_CLIENT_SECRET?.toString() ?? ''
+
+const clientId = "8a13e643a21789547ad0"
+const clientSecret = "b8419ff342c26e0af5d68cf999f8a57f26afe6f0"
+
+const oAuthMobile = new OAuthApp({
+  clientId,
+  clientSecret,
+  defaultScopes: ["user:email"]
+});
+
+// **** Exports **** //
+
+export default oAuthMobile
