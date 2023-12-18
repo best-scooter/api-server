8833f48359c04e074c7d3c2bd6502c614b688e7e
Adam Esbjörnsson <ades22@student.bth.se>
2023-12-18 07:31:32 +0100

    add env vars for pricing

--- src/constants/EnvVars.ts
+++ src/constants/EnvVars.ts
@@ -6,7 +6,7 @@
 
 export default {
   NodeEnv: (process.env.NODE_ENV ?? ''),
-  Port: (process.env.PORT ?? 0),
+  Port: (parseInt(process.env.PORT ?? '')),
   DbServer: (process.env.DATABASE_SERVER ?? ''),
   DbDatabase: (process.env.DATABASE_DATABASE ?? ''),
   DbUsername: (process.env.DATABASE_USERNAME ?? ''),
@@ -15,5 +15,8 @@ export default {
   GithubAppId: (process.env.GITHUB_APP_ID ?? ''),
   GithubPrivateKey: (process.env.GITHUB_PRIVATE_KEY ?? ''),
   OAuthClientId: (process.env.OAUTH_CLIENT_ID ?? ''),
-  OAuthClientSecret: (process.env.OAUTH_CLIENT_SECRET ?? '')
+  OAuthClientSecret: (process.env.OAUTH_CLIENT_SECRET ?? ''),
+  PriceInitial: (parseFloat(process.env.PRICE_INITIAL ?? '')),
+  PriceDistance: (parseFloat(process.env.PRICE_DISTANCE ?? '')),
+  PriceTime: (parseFloat(process.env.PRICE_TIME ?? ''))
 } as const;
